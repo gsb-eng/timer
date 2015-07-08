6871753df27781684abd83d65279ca7123c52962
Srinivas Garlapati <srinivasa.b.garlapati@gmail.com>
2015-07-08 23:40:44 +0530

    Added more time metrics

--- timer/timer.py
+++ timer/timer.py
@@ -2,14 +2,32 @@
 Timer module.
 """
 
+import enum
 import time
+
+
+NANO_SECONDS = 'nsec'
+MILLI_SECONDS = 'msec'
+SECONDS = 'sec'
+MINUTES = 'min'
+
+
+class TimeMetric(enum.Enum):
+    sec = 1
+    nsec = 1 / pow(10, 9)
+    msec = 1 / pow(10, 3)
+    min = 1 * 60
+
+
+def get_time():
+    return time.time()
 
 
 class Timer(object):
     """
     Timer class.
     """
-    def __init__(self, time_in=None):
+    def __init__(self, time_in='sec'):
         """
         @param time_in string: This defines the time metric min/sec/msec/nsec.
         """
@@ -17,10 +35,12 @@ class Timer(object):
         self.start_time = None
         self.end_time = None
 
+    def __time_in_metric(self):
+        return get_time() * float(TimeMetric[self.time_in].value)
+
     def __enter__(self):
-        self.start_time = time.time()
+        self.start_time = self.__time_in_metric()
 
     def __exit__(self, exc_type, exc_value, traceback):
-        self.end_time = time.time()
-        print self.end_time - self.start_time
-
+        self.end_time = self.__time_in_metric()
+        print self.end_time - self.start_time, self.time_in
